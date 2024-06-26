7f25102f7682ce5e729e76a2a8f56b4950c27029
Li Zhao <lzhao@bglcorp.com.au>
2024-06-26 15:35:18 +1000

    Initial commit

---
+++ test/saas_workshop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SaasWorkshop from '../lib/saas_workshop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/saas_workshop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SaasWorkshop.SaasWorkshopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
